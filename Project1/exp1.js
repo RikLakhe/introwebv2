ab680e6d976e7f72273f378d3b444c2167e96a5c
riklakhe <rikeshlakhe@gmail.com>
2018-08-05 14:02:30 +0545

    2d done

---
+++ Project1/exp1.js
@@ -0,0 +1,14 @@
+const monthlyRent = 500;
+
+const yearlyRent = monthlyRent * 12;
+
+console.log(yearlyRent);
+
+
+const firstName = 'Rikesh';
+
+const lastName = 'Shrestha';
+
+const joinString = `Hello! Mr. ${firstName} ${lastName}`;
+// `` is the StringTemplate 
+console.log(joinString);
